6af1769b88cac17f849cbaa9c9753b1c503121c1
tinysquirrelton <soheil.saadati@hotmail.com>
2021-03-05 23:39:47 +0100

    init done 3/3

---
+++ src/components/authentication/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./style.scss";
+
+export default class Authentication extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return 1;
+  }
+}
